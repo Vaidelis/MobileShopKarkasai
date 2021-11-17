566f3baf7a90bc1a5cc21832728d9aefecc76b58
Vaidas <61739600+Vaidelis@users.noreply.github.com>
2021-11-17 18:49:34 +0200

    3 lab gynimas

--- resources/views/groupComments.blade.php
+++ resources/views/groupComments.blade.php
@@ -5,7 +5,16 @@
     <div class="top">
         <div class="row justify-content-center">
             <div class="col-md-8" style="color:antiquewhite">
+                <!---
+                foreach ($group as $grou)
+                    @oreach ($grou->comment as $comment)
+                        <table> <th>$grou->name</th></table>
+                        <td> $comment->user_username</td>
+                    endforeach
+                endforeach -->
+                    <table> <th>{{$group->name}}</th></table>
                 <table class="table table-striped" >
+
                     <thead>
                     <th>Slapyvardis</th>
                     <th>Komentaras</th>
